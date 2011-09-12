6496496b4b6fec3db9c2b3dfe35466b594453d84
Tony Karppinen <tony.karppinen@gmail.com>
2011-09-12 19:33:52 +0300

    Crude user authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Veggie::Application.routes.draw do
   resources :restaurants
   resources :users
-
+  resources :sessions  
+  
+  match "/logout" => "sessions#destroy", :as => "logout"
+  match "/login" => "sessions#new", :as => "login"
+  match "/main" => "restaurants#index", :as => "main"
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,6 +57,7 @@ Veggie::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'restaurants#index'
 
   # See how all your routes lay out with "rake routes"
 
