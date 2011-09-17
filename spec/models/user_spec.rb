7faf1868a78fa451ccd606f00799c4f2364f3e15
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2011-09-17 15:11:03 +0300

    Added a spec for testing user model so that attributes cannot be mass assigned.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  context "validations" do
+  context "validations on create" do
     it "should save a user with a username, password and email" do
       user = FactoryGirl.build(:user)
       user.save.should be_true
@@ -26,6 +26,20 @@ describe User do
       user = FactoryGirl.build(:user, :email => "userATuser.org")
       user.save.should be_false
     end
+  end  
+  
+  context "mass assigning attributes" do
     
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+    
+    it "should not allow mass assign of a new id" do
+      expect { @user.update_attributes({:id => 666}) }.to_not change { @user.id }
+    end
+    
+    it "should not allow mass assign of timestamps" do
+      expect { @user.update_attributes({:created_at => Time.now}) }.to_not change { @user.created_at }
+    end
   end
 end
