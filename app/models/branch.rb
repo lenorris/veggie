e78f003a95be939a6f4a532cba46d43b4f1837cc
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2011-09-23 00:32:38 +0300

    Added a callback to fetch coordinates for branch.
    Coordinates are fetched before save if model is created or if address is
    changed on update.
    
    Errors are only logged, we should decide later if we want to do
    something else with them.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,5 +1,24 @@
 class Branch < ActiveRecord::Base
+   
   belongs_to :restaurant
   validates :street_address, :city, :presence => true
+  before_save :geocode, :if => :needs_geocoding?
+  attr_accessible :street_address, :city, :phone, :email, :business_hours
+
+  private
+  
+  def geocode
+    begin
+      self.latitude, self.longitude = Geocoder.get_latlng("#{street_address}, #{city}")
+    rescue Geocoder::GeocoderError => e
+      logger.error e
+    end
+  end
+  
+  # lat and lng should be fetched if record is new or address is changed
+  def needs_geocoding?
+    self.new_record? || self.city_changed? || self.street_address_changed?
+  end
+    
 end
  
--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
 require 'spec_helper'
+require 'geocoder'
 
 describe Branch do
+  
   context "validations" do
 
     it "should not save a branch without a street address" do
@@ -12,5 +15,82 @@ describe Branch do
       branch = FactoryGirl.build(:branch, :city => nil)
       branch.save.should be_false
     end
+    
   end
+  
+  
+  context "mass assigning attributes" do
+    
+    before(:each) do
+      @branch = FactoryGirl.create(:branch)
+    end
+    
+    it "should not allow to change id" do
+      expect { @branch.update_attributes({:id => 666}) }.to_not change { @branch.id }
+    end
+    
+    it "should not allow to change restaurant id" do
+      expect { @branch.update_attributes({:restaurant_id => 666}) }.to_not change { @branch.restaurant_id }
+    end
+    
+    it "should not allow to change latitude" do
+      expect { @branch.update_attributes({:latitude => 666}) }.to_not change { @branch.latitude }
+    end
+    
+    it "should not allow to change latitude" do
+      expect { @branch.update_attributes({:longitude => 666}) }.to_not change { @branch.longitude }
+    end
+  end
+  
+  describe "geocoding" do
+    
+    before (:all) do
+      @lat = 60.16794829999999
+      @lng = 24.93563410
+    end
+    
+    context "on create" do     
+     it "should try to geocode branches address on create" do
+        Geocoder.should_receive(:get_latlng).with("Annankatu 29, Helsinki").and_return([@lat, @lng])
+        branch = FactoryGirl.create(:branch, :city => 'Helsinki', :street_address => 'Annankatu 29')
+        branch.latitude.should == @lat
+        branch.longitude.should == @lng    
+      end
+      
+      it "should log error if geocoding fails" do
+        Geocoder.should_receive(:get_latlng).and_raise(Geocoder::RequestDeniedError)
+        Rails.logger.should_receive(:error)
+        branch = FactoryGirl.create(:branch, :city => 'Helsinki', :street_address => 'Annankatu 29')
+      end
+        
+    end
+    
+    context "on update" do
+      
+      before(:each) do
+        @branch = FactoryGirl.create(:branch, :street_address => 'Annankatu 29', :city => 'Helsinki')  
+      end
+      
+      it "should try to geocode branch address on update if street address was changed" do
+        Geocoder.should_receive(:get_latlng).with("Change Street 123, Helsinki").and_return([@lat, @lng])
+        @branch.update_attributes :street_address => "Change Street 123"
+        @branch.latitude.should == @lat
+        @branch.longitude.should == @lng
+      end
+      
+      it "should try to geocode branch address on update if city was changed" do
+        Geocoder.should_receive(:get_latlng).with("Annankatu 29, Espoo").and_return([@lat, @lng])
+        @branch.update_attributes :city => "Espoo"
+        @branch.latitude.should == @lat
+        @branch.longitude.should == @lng
+      end
+            
+      it "should not try to geocode branch address if address wasn't changed" do
+        Geocoder.should_not_receive(:get_latlng)
+        @branch.update_attributes :phone => "0700-666666"
+      end
+    end
+  end
+
+  
 end
