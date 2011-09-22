b2d41839408fae6abc9837dc93ee5616114b9d18
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2011-09-22 21:33:30 +0300

    Geocoder for address geocoding. Uses Google Geocoding API.

---
+++ spec/lib/geocoder_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+require 'geocoder'
+
+describe Geocoder do
+  
+  before(:all) do
+    @address = 'Annankatu 29, Helsinki, Finland'
+    @lat = 60.16794829999999
+    @lng = 24.93563410
+    @responses = {:ok => "OK", :empty => "ZERO_RESULTS", :over_limit => "OVER_QUERY_LIMIT", :denied => "REQUEST_DENIED", :invalid => "INVALID_REQUEST"}
+  end
+  
+  context "with a ok response" do
+    
+    before(:each) do
+    end
+    
+    it "should parse the result and return lat and lng if results are unambiguous" do
+      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(1, @responses[:ok], @lat, @lng))
+      lat, lng = Geocoder.get_latlng(@address)
+      lat.should be_within(0.01).of(@lat)
+      lng.should be_within(0.01).of(@lng)
+    end
+    
+    it "should raise an Error if the results are ambiguous" do
+      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(2, @responses[:ok], @lat, @lng))
+      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::AddressAmbiguousError)
+    end
+  
+  end
+  
+  context "with a zero results response" do
+    
+    it "should raise an Error if the results are empty" do
+      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:empty], @lat, @lng))
+      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::EmptyResultsError)
+    end
+    
+  end
+  
+  context "with over query limit response" do
+    
+    it "should raise an Error if the query limit to google api is exceeded" do
+      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:over_limit], @lat, @lng))
+      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::OverQueryLimitError)
+    end
+    
+  end
+  
+  context "with a request denied response" do
+    
+    it "should raise an Error if the response is request denied" do
+      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:denied], @lat, @lng))
+      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::RequestDeniedError)
+    end    
+    
+  end
+  
+  context "with a invalid request response" do
+    
+    it "should raise an Error if the response is invalid request" do
+      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:invalid], @lat, @lng))
+      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::InvalidRequestError)
+    end  
+  
+  end
+  
+end
