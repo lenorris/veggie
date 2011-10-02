968edbaf97019e78aa159d7a6194c7834e4c9174
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2011-10-02 14:55:07 +0300

    Fixed accetance test's login phase.

--- spec/acceptance/restaurants_spec.rb
+++ spec/acceptance/restaurants_spec.rb
@@ -10,8 +10,8 @@ describe 'adding a restaurant', :type => :request  do
  
 it 'should create a restaurant' do
     visit '/'
-    fill_in 'username', :with => @username
-    fill_in 'password', :with => @password
+    fill_in 'user_login', :with => @username
+    fill_in 'user_password', :with => @password
     click_button 'Log in'
     visit '/restaurants'
     click_link 'New Restaurant'
