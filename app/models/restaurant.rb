38d8dd4cd5426d0de1eb49a28a8b217480a0647e
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2011-10-07 20:03:19 +0300

    Added a callback to restaurant model so that it prefixes website url with http:// if protocol was not given by user.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,11 @@ class Restaurant < ActiveRecord::Base
   validates_presence_of :name, :info
   has_many :branches, :dependent => :destroy
   accepts_nested_attributes_for :branches
+  before_save :add_protocol_to_website
   
+  private
+  
+  def add_protocol_to_website
+    self.website = /^http/.match(self.website) ? self.website : "http://#{self.website}"
+  end
 end
