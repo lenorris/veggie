e2acdf2884e57d99d3495d4113dea9e864cd8645
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2011-10-09 15:14:39 +0300

    Refactored login helper. It now takes a user model as parameter.

--- spec/acceptance_spec_helper.rb
+++ spec/acceptance_spec_helper.rb
@@ -1,6 +1,6 @@
-def login(username, password)
+def login(user)
   visit '/'
-  fill_in 'user_login', :with => username
-  fill_in 'user_password', :with => password
+  fill_in 'user_login', :with => user.username
+  fill_in 'user_password', :with => user.password
   click_button 'Log in'
 end
