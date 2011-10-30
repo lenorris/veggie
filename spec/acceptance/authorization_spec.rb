912a90cb6f2c5eb446971f6b918ecc1b0ff90310
Tony Karppinen <tony.karppinen@gmail.com>
2011-10-30 22:41:51 +0200

    Fixed specs

--- spec/acceptance/authorization_spec.rb
+++ spec/acceptance/authorization_spec.rb
@@ -4,8 +4,7 @@ describe 'authorization', :type => :request  do
   
   it "should redirect to root page if user doesn't have the correct rights" do
     visit restaurants_path
-    #click_link 'New Restaurant'
-    click_link 'restaurants/new'
+    click_link 'New Restaurant'
     current_path.should == root_path
     page.should have_selector('#alert')
   end
