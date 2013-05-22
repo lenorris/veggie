546db508e79813c46309f36c1578837d7fb59596
= <mikko.kangasaho@gmail.com>
2013-05-22 15:10:37 +0300

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -65,12 +65,12 @@ ActiveRecord::Schema.define(:version => 20111029131927) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
