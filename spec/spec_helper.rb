a8eba32391eefd36c78beab808f090e0959055de
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2013-05-22 17:01:20 +0300

    Capybara upgrade issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require 'rspec/rails'
 
 # Add this to load Capybara integration:
 require 'capybara/rspec'
-require 'capybara/rails'
+#require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -22,6 +22,8 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
+  
+  config.include Capybara::DSL
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
