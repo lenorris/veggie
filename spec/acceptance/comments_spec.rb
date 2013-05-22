e8cfdeb679abeb94cae1b64134a9aeede22c2b3c
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2013-05-22 17:51:35 +0300

    Fixed broken comments specs.

--- spec/acceptance/comments_spec.rb
+++ spec/acceptance/comments_spec.rb
@@ -2,34 +2,67 @@ require 'spec_helper'
 require 'acceptance_spec_helper'
  
 describe 'comments', :type => :request  do
-  
+    
   before(:each) do
     @user = FactoryGirl.create(:user, :username => 'haxxxxxor', :password => 't0ps3cR3tZZZ' )
     @restaurant = FactoryGirl.create(:branch).restaurant
     @comment_body = "Had currytofu and it was the best meal ever. My friend had sataytofu and it was awesome too. I will definitely visit again!"
-  end
- 
-  it 'should create a comment and show it and a thanks for commenting -note', :js => true do
     login(@user)
     visit restaurant_path(@restaurant)
-    fill_in "comment_body", :with => @comment_body
-    comment_count =  page.all("#comments .comment").size
-    click_button I18n.t('helpers.submit.comment.create')
-    sleep 1 #TODO: find a better fix for this
-    page.all("#comments .comment").size.should == comment_count + 1
-    page.all('#comments .comment').last.should have_content(@comment_body)
-    page.find('#notice').should have_content(I18n.t('comments.thanks'))
-    page.find('#new_comment').should_not be_visible # should hide the form
   end
   
-  it 'should show errors and not create a comment if body is empty', :js => true do
-    login(@user)
-    visit restaurant_path(@restaurant)
-    comment_count =  page.all("#comments .comment").size
-    click_button I18n.t('helpers.submit.comment.create')
-    page.all("#comments .comment").size.should == comment_count
-    page.find('#error_explanation').should have_content(I18n.t('errors.messages.blank'))
-    Comment.count.should == 0
+  
+  context 'valid comment' do
+    before(:each) do
+      fill_in "comment_body", :with => @comment_body
+    end
+    
+    it 'should create a comment' do
+      expect{ click_comment_button }.to change(Comment, :count).by(1)
+    end
+    
+    context 'after creation' do
+      before(:each) do
+        click_comment_button
+      end
+      
+      it 'should show the new comment', :js => true do
+        expect(all('.comment').last).to have_content(@comment_body)
+      end
+      
+      it 'should snow a thanks for commenting -note', :js => true do
+        expect(page.find('#notice')).to have_content(I18n.t('comments.thanks'))
+      end
+      
+      it 'should not show the comment form', :js => true do
+        expect(page).to have_no_selector('#new_comment')
+      end
+      
+    end
   end
   
+  context 'invalid comment' do
+    before(:each) do
+      click_button I18n.t('helpers.submit.comment.create')
+    end
+    
+    it 'should not show a new comment', :js => true do
+      expect(page.all("#comments .comment").size).to eq 0
+    end
+    
+    it 'should show an error message', :js => true do
+      expect(page.find('#error_explanation')).to have_content(I18n.t('errors.messages.blank'))
+    end
+    
+    it 'should show errors and not create a comment if body is empty', :js => true do
+      expect(Comment.count).to eq 0
+    end
+  end
+  
+  
+  private
+    def click_comment_button
+      click_button I18n.t('helpers.submit.comment.create')
+      sleep 1 #TODO find a better way
+    end
 end
