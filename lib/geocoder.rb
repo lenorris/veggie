a9798d70ba70f3258b35c04c4d44a7c52ab31e56
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2013-05-27 23:33:22 +0300

    Possibility to use multiple backends/adapter for geocoding.
    Geocoder module now uses all defined adapters and tries to
    geocode with each of them until it receives a succesful response.

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -1,39 +1,8 @@
 require "net/http"
 require "uri"
+require 'google_geocoder'
 
 module Geocoder
-  
-  
-  def self.get_latlng(address)
-    
-    return mock_answer if Rails.env.test?
-    
-    url_encoded_address = URI.encode(address)
-    base_uri = ("http://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_address}&sensor=false&region=fi")
-    uri = URI.parse(base_uri)
-    response = JSON.parse(Net::HTTP.get(uri))
-    case response['status']
-    when "OK" # indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned.
-      results = response['results']
-      if results.size == 1
-        latlng = results.first['geometry']['location']
-        return latlng['lat'], latlng['lng']
-      else
-        raise AddressAmbiguousError
-      end
-    when "ZERO_RESULTS" # indicates that the geocode was successful but returned no results.
-      raise EmptyResultsError, "No results found with given address."
-    when "OVER_QUERY_LIMIT" # indicates that you are over your quota.
-      raise OverQueryLimitError, "Google Geocoding API query limit exceeded."
-    when "REQUEST_DENIED" # indicates that your request was denied, generally because of lack of a sensor parameter.
-      raise RequestDeniedError, "Request denied. Check that URL is correct. URL was #{base_uri}"
-    when "INVALID_REQUEST" # generally indicates that the query (address or latlng) is missing.
-      raise InvalidRequestError, "Request was invalid. Check that URL is correct. URL was #{base_uri} "
-    end
-    
-  end
-
-  # Errors thrown (TODO: rethink this, might not be the best way to handle this)
   class GeocoderError < StandardError
   end
   class AddressAmbiguousError < GeocoderError
@@ -47,11 +16,29 @@ module Geocoder
   class InvalidRequestError < GeocoderError
   end
   
+  def self.get_latlng(street_address, city, country)
+    return mock_answer if Rails.env.test?
+    adapters.each do |adapter|
+      begin
+        return adapter.geocode(street_address, city, country)
+      rescue GeocoderError => e
+        # TODO: log
+      end
+    end
+    raise GeocoderError, "All geocoder adapters failed."
+  end
+
+
+  
   private
   
-  def self.mock_answer
-    [60.16794829999999, 24.93563410]
-  end
+    def self.mock_answer
+      [60.16794829999999, 24.93563410]
+    end
+
+    def self.adapters
+      return [Geocoder::GoogleGeocoder.new]
+    end
   
 end
 
--- spec/lib/geocoder_spec.rb
+++ spec/lib/geocoder_spec.rb
@@ -3,80 +3,63 @@ require 'geocoder'
 
 describe Geocoder do
   
-  before(:all) do
-    @address = 'Annankatu 29, Helsinki, Finland'
-    @lat = 60.16794829999999
-    @lng = 24.93563410
-    @responses = {:ok => "OK", :empty => "ZERO_RESULTS", :over_limit => "OVER_QUERY_LIMIT", :denied => "REQUEST_DENIED", :invalid => "INVALID_REQUEST"}
-  end
-  
   before(:each) do
-    # normally during tests geocoding is stopped at the beginning and mock answer returned
-    # to circumvent that we stub Rails.env
+    @street_address = 'Annankatu 29'
+    @city = 'Helsinki' 
+    @country = 'fi'
+    @expected_lat = 60.16794829999999
+    @expected_lng = 24.93563410
+    @expected_coordinates = [@expected_lng, @expected_lng]
     Rails.env.stub(:test?).and_return(false)
+    @google_adapter = double('google adapter')
+    @yahoo_adapter = double('yahoo adapter')
+    Geocoder.stub(:adapters).and_return([@google_adapter, @yahoo_adapter])
+    @geocode = lambda { Geocoder.get_latlng(@street_address, @city, @country) }
   end
-  
-  context "with a ok response" do
 
-    it "should parse the result and return lat and lng if results are unambiguous" do
-      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(1, @responses[:ok], @lat, @lng))
-      lat, lng = Geocoder.get_latlng(@address)
-      lat.should be_within(0.01).of(@lat)
-      lng.should be_within(0.01).of(@lng)
+
+
+  context "first geocoding adapter fails" do
+    before(:each) do
+      @google_adapter.stub(:geocode).and_raise(Geocoder::GeocoderError)
     end
-    
-    it "should raise an Error if the results are ambiguous" do
-      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(2, @responses[:ok], @lat, @lng))
-      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::AddressAmbiguousError)
+    it "should call the remaining adapters" do
+      @yahoo_adapter.should_receive(:geocode)
+      @geocode.call
     end
-  
-  end
-  
-  context "with a zero results response" do
-    
-    it "should raise an Error if the results are empty" do
-      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:empty], @lat, @lng))
-      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::EmptyResultsError)
+    it "should return what the next succeeding adapter returns" do
+      @yahoo_adapter.stub(:geocode).and_return(@expected_coordinates)
+      expect(@geocode.call).to eq @expected_coordinates
     end
-    
   end
-  
-  context "with over query limit response" do
-    
-    it "should raise an Error if the query limit to google api is exceeded" do
-      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:over_limit], @lat, @lng))
-      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::OverQueryLimitError)
+
+  context "first geocoding adapter succeeds" do
+    before(:each) do
+      @google_adapter.stub(:geocode).with(@street_address, @city, @country).and_return(@expected_coordinates)
     end
-    
+
+    it "should return the value returned by the adapter" do
+      expect(@geocode.call).to eq @expected_coordinates
+    end
+
+    it "should try to geocode only first when it succeeds" do
+      @yahoo_adapter.should_not_receive(:geocode)
+      @geocode.call
+    end
   end
-  
-  context "with a request denied response" do
-    
-    it "should raise an Error if the response is request denied" do
-      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:denied], @lat, @lng))
-      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::RequestDeniedError)
-    end    
-    
+
+  context "all adapters fail" do
+    it "should raise a GeocoderError" do
+      @google_adapter.stub(:geocode).and_raise(Geocoder::GeocoderError)
+      @yahoo_adapter.stub(:geocode).and_raise(Geocoder::GeocoderError)
+      expect(@geocode).to raise_error(Geocoder::GeocoderError)
+    end
   end
-  
-  context "with a invalid request response" do
-    
-    it "should raise an Error if the response is invalid request" do
-      Net::HTTP.should_receive(:get).and_return(mock_google_api_response(0, @responses[:invalid], @lat, @lng))
-      lambda {Geocoder.get_latlng(@address)}.should raise_error(Geocoder::InvalidRequestError)
-    end  
-  
-  end
-  
+
   context "mock responses" do
-    before (:all) do
-      @lat = 60.16794829999999
-      @lng = 24.93563410
-    end
-    
     it "should return Bamboo Centers coordinates when rails environment is test" do
       Rails.env.stub!(:test?).and_return(true) # stubbed because we stub it as false for all tests earlier
-      Geocoder.get_latlng('Trolololo').should == [@lat, @lng]
+      Geocoder.get_latlng('Trolololo', 'what', 'ever').should eq [@expected_lat, @expected_lng]
     end   
     
   end
