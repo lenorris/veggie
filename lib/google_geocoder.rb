3c45cee7e1585dd0773351adbbf031308370e710
Mikko Kangasaho <mikko.kangasaho@gmail.com>
2013-06-08 15:35:28 +0300

    Make GoogleGeocoder use the GoogleParser.

--- lib/google_geocoder.rb
+++ lib/google_geocoder.rb
@@ -1,47 +1,25 @@
 require 'http_client'
+require 'google_parser'
 module Geocoder
   class GoogleGeocoder
     BASE_URL = "http://maps.googleapis.com/maps/api/geocode/json"
-    RESPONSES = {:ok => "OK", :empty => "ZERO_RESULTS", :over_limit => "OVER_QUERY_LIMIT", :denied => "REQUEST_DENIED", :invalid => "INVALID_REQUEST"}
 
     def geocode(street_address, city, country)
-      http_client = HttpClient.new
       response = http_client.get(BASE_URL, params(street_address, city, country))
-      response = parse_response(response)
-      status = response['status']
-      validate_status(status)
-      latlng = response['results'].first['geometry']['location']
-      return latlng['lat'], latlng['lng']
+      return parser.parse(response)
     end
     
     private
       def params(street_address, city, country)
         {:address => "#{street_address},#{city}", :region => country, :sensor => false}
       end
-      
-      def parse_response(response)
-        begin
-          JSON.parse(response)
-        rescue JSON::ParserError => e
-          raise GeocoderError, e
-        end
+
+      def http_client()
+        HttpClient.new
       end
 
-      def validate_status(status)
-        case status
-        when RESPONSES[:ok] 
-          return
-        when RESPONSES[:empty]
-          raise EmptyResultsError, "No results found with given address."
-        when RESPONSES[:over_limit] # indicates that you are over your quota.
-          raise OverQueryLimitError, "Google Geocoding API query limit exceeded."
-        when RESPONSES[:denied] # indicates that your request was denied, generally because of lack of a sensor parameter.
-          raise RequestDeniedError, "Request denied. Check that URL is correct."
-        when RESPONSES[:invalid] # generally indicates that the query (address or latlng) is missing.
-          raise InvalidRequestError, "Request was invalid."
-        else
-          raise GeocoderError, "Got an response #{status} from Google. Don't understand it."
-        end
+      def parser()
+        Geocoder::GoogleParser.new
       end
-  end
+    end
 end
--- spec/lib/google_geocoder_spec.rb
+++ spec/lib/google_geocoder_spec.rb
@@ -5,78 +5,70 @@ describe Geocoder::GoogleGeocoder do
 
   # https://developers.google.com/maps/documentation/geocoding/
   before(:each) do
-    @base_url = "http://maps.googleapis.com/maps/api/geocode/json"
     @street_address = 'Annankatu 29'
     @city = 'Helsinki' 
     @country = 'fi'
-    @responses = {:ok => "OK", :empty => "ZERO_RESULTS", :over_limit => "OVER_QUERY_LIMIT", :denied => "REQUEST_DENIED", :invalid => "INVALID_REQUEST"}
     @geocoder = Geocoder::GoogleGeocoder.new
-    @expected_lat, @expected_lng = 60.16783980, 24.93578350
-    @http_client = mock("http_client")
-    HttpClient.stub(:new).and_return @http_client
     @geocode = lambda { @geocoder.geocode(@street_address, @city, @country) }
-    
+
+    @responses = {:ok => "OK", :empty => "ZERO_RESULTS", :over_limit => "OVER_QUERY_LIMIT", :denied => "REQUEST_DENIED", :invalid => "INVALID_REQUEST"}
+
+    @http_client = mock("http_client")
+    @parser = mock('google_parser')
+    @parser.stub(:parse)
+    HttpClient.stub(:new).and_return @http_client
+    Geocoder::GoogleParser.stub(:new).and_return @parser
   end
   
-  context "url and parameters" do
-    it "are built correctly" do
+  context "http client" do
+    it "is called with correct parameters" do
+      @base_url = "http://maps.googleapis.com/maps/api/geocode/json"
       parameters = {:address => "#{@street_address},#{@city}", :region => @country, :sensor => false} 
       @http_client.should_receive(:get).with(@base_url, parameters).and_return(mock_google_api_response(1, @responses[:ok], @expected_lat, @expected_lng))
       @geocode.call
     end
   end
-    
-  context "response is ok" do
+
+  context "parser" do
+
     before(:each) do
-      @http_client.stub(:get).and_return(mock_google_api_response(1, @responses[:ok], @expected_lat, @expected_lng))
-      @lat, @lng = @geocode.call
-    end
-    
-    it "returns correct lat" do
-      expect(@lat).to be_within(0.01).of(@expected_lat)
-    end
-    
-    it "returns correct lng" do
-      expect(@lng).to be_within(0.01).of(@expected_lng)
-    end
-  end
-  
-  context "response is not ok" do
-    
-    it 'throws an GeocodingException with invalid json' do
-      @http_client.stub(:get).and_return('trololoo')
-      expect(@geocode).to raise_error(Geocoder::GeocoderError)
+      @response = mock_google_api_response(1, @responses[:ok], @expected_lat, @expected_lng)
+      @http_client.stub(:get).and_return(@response)
     end
 
-    it 'throws an exception with empty results' do
-      @http_client.stub(:get).and_return(mock_google_api_response(0, @responses[:empty], @lat, @lng))
-      expect(@geocode).to raise_error(Geocoder::EmptyResultsError)
+    it "calls it with the response from http client" do
+      @parser.should_receive(:parse).with(@response)
+      @geocode.call
     end
 
-    it 'raises an exception with ambiguous results' do
-      @http_client.stub(:get).and_return(mock_google_api_response(2, @responses[:ok], @lat, @lng))
-    end
-    
-    it "should raise an Error if the query limit to google api is exceeded" do
-      @http_client.stub(:get).and_return(mock_google_api_response(0, @responses[:over_limit], @lat, @lng))
-      expect(@geocode).to raise_error(Geocoder::OverQueryLimitError)
-    end
-        
-    it "should raise an Error if the response is request denied" do
-      @http_client.stub(:get).and_return(mock_google_api_response(0, @responses[:denied], @lat, @lng))
-      expect(@geocode).to raise_error(Geocoder::RequestDeniedError)
-    end    
-    
+    context "parser returns lat and lng" do
+      before(:each) do
+        @expected_lat, @expected_lng = 60.16783980, 24.93578350
+        @parser.stub(:parse).and_return([@expected_lat, @expected_lng])
+        @http_client.stub(:get).and_return(mock_google_api_response(1, @responses[:ok], @expected_lat, @expected_lng))
+        @lat, @lng = @geocode.call
+      end
       
-    it "should raise an Error if the response is invalid request" do
-      @http_client.stub(:get).and_return(mock_google_api_response(0, @responses[:invalid], @lat, @lng))
-      expect(@geocode).to raise_error(Geocoder::InvalidRequestError)
+      it "returns correct lat" do
+        expect(@lat).to be_within(0.01).of(@expected_lat)
+      end
+      
+      it "returns correct lng" do
+        expect(@lng).to be_within(0.01).of(@expected_lng)
+      end
+    end
+  
+    context "response is not ok" do
+      before(:each) do
+        @exception = Geocoder::GeocoderError.new
+        @parser.stub(:parse).and_raise(@exception)
+      end
+
+      it 'propagates the exception' do
+        expect(@geocode).to raise_error(@exception)
+      end
     end
 
-    it "should raise geocoding exception when it receives an unknown status" do
-      @http_client.stub(:get).and_return(mock_google_api_response(0, "trololoo", @lat, @lng))
-      expect(@geocode).to raise_error(Geocoder::GeocoderError)
-    end
   end
-  
+
 end
